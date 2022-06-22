52c964cd54605f637399bb64e1ad56ad8d77c716
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-06-21 23:09:28 -0500

    try

--- tareaReto1/fetch.js
+++ tareaReto1/fetch.js
@@ -1,10 +1,11 @@
-console.log('Validacion ');
+console.log("Validacion ");
 
 const form = document.getElementById("form").value;
-const nombre = document.getElementById("name").value;
-const apellido = document.getElementById("lastname").value;
-const correo = document.getElementById("email").value;
+const nameOne = document.getElementById("name").value;
+const lastName = document.getElementById("lastname").value;
+const email = document.getElementById("email").value;
 const age = document.getElementById("edad").value;
+<<<<<<< Updated upstream
 const formValidacion = () => {
   console.log("formValidacion");
         if (
@@ -22,5 +23,30 @@ const formValidacion = () => {
                msgName.innerHTML="";
             msgApellido.innerHTML="";
              }
+=======
 
-  }
+//declara div
+// const er =
+//   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const formValidacion = () => {
+  console.log("formValidacion");
+if (nameOne.length < 3) {
+  console.log("faliure");
+  alert("El nombre y apellido minimo tiene que contener 3 letras");
+  msgName.innerHTML = "El nombre minimo tiene que contener 3 letras ";
+} else {
+  console.alert("success");
+  msgName.innerHTML = "";
+}
+if (lastName.length < 3) {
+  console.log("faliure");
+  alert("El nombre y apellido minimo tiene que contener 3 letras");
+  msgApellido.innerHTML = "El apellido minimo tiene que contener 3 letras ";
+} else {
+  console.alert("success");
+  msgApellido.innerHTML = "";
+}
+
+};
+>>>>>>> Stashed changes
+
